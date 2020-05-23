333f9383ea969c5f3fe04c361860d74efef76cea
reemalmashhur <ramalqahtani881@hotmail.com>
2020-05-23 21:36:09 +0300

    add ',' into string line

--- test.cpp
+++ test.cpp
@@ -15,12 +15,12 @@ void printInput (vector<int> v){
 void testFile (){
     cout<<"---------------------------"<<'\n';
     cout<<"your programming  ";
-    string line = "0,1,0,0,0,0";
+    string line = "0,1,0,0,0,0,";
     vector<int> solution_vect{0,1,0,0,0,0};
     vector<int> testing;
     testing = parsingLine(line);
 
-    if(testing != solution_vect ){
+    if(testing != solution_vect  ){
         cout<< "filed"<<'\n';
         printInput(testing);
         cout<<line;
